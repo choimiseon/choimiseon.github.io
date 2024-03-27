2188b006dd7c0668acbd765aea7881886683f4fa
choimiseon <akrekffps1@nate.com>
2024-03-27 18:15:14 +0900

    블로그 메인 변경

--- cmsblog/src/App.tsx
+++ cmsblog/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,10 @@ function App() {
       </header>
     </div>
   );
-}
-
+} */
+const App = () => {
+  return (
+    <h1>GitHub Pages로 리액트 프로젝트 배포</h1>
+  );
+};
 export default App;
